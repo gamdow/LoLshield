98319e40df38906139636b9f8ace0aa87569801f
gamdow <gmd1n15@soton.ac.uk>
2018-04-05 19:56:16 +0100

    shell sort and some crappy encapsulation

--- sorting_demo/sort.h
+++ sorting_demo/sort.h
@@ -5,48 +5,39 @@
 
 typedef uint8_t element_t;
 
-element_t elements[N_COLUMNS];
-float reads[N_COLUMNS];
-float writes[N_COLUMNS];
-int read_count = 0;
-int write_count = 0;
-
-void init_elements() {
-  for(byte i = 0; i < N_COLUMNS; ++i ) {
-    elements[i] = random(0, N_ROWS);
-    reads[i] = 0;
-    writes[i] = 0;
-    read_count = 0;
-    write_count = 0;
-  }
-}
-
-void decay_hightlights(float decay) {
-  for(byte i = 0; i < N_COLUMNS; ++i ) {
-    reads[i] *= decay;
-    writes[i] *= decay;
-    read_count = 0;
-    write_count = 0;
-  }
-}
-
 struct SortBase {
-  void SetFinished(bool value) {
-    finished = value;
-  }
-
-  bool GetFinished() const {
-    return finished;
-  }
-
   SortBase() {
-    init();
+    initElements();
     finished = true;
   }
 
-  virtual void init() {
-    init_elements();
+  void initElements() {
+    for(byte i = 0; i < N_COLUMNS; ++i) {
+      elements[i] = random(0, N_ROWS);
+      reads[i] = 0.f;
+      writes[i] = 0.f;
+    }
+    read_count = 0;
+    write_count = 0;
+  }
+
+  void decayHightlights(float decay) {
+    for(byte i = 0; i < N_COLUMNS; ++i) {
+      reads[i] *= decay;
+      writes[i] *= decay;
+    }
+    read_count = 0;
+    write_count = 0;
+  }
+
+  inline void step() {stepImpl();}
+  inline void setFinished(bool value) {finished = value;}
+  inline bool getFinished() const {return finished;}
+
+  void init() {
+    initElements();
     finished = false;
+    initImpl();
   }
 
   element_t get(int i) const {
@@ -61,28 +52,35 @@ struct SortBase {
     elements[i] = e;
   }
 
-  virtual void step() = 0;
+  static element_t elements[N_COLUMNS];
+  static float reads[N_COLUMNS];
+  static float writes[N_COLUMNS];
+  static int read_count;
+  static int write_count;
 
+private:
+  virtual void initImpl() = 0;
+  virtual void stepImpl() = 0;
   bool finished;
 };
 
-struct BubbleSortData {
-  int8_t min_idx;
-  int8_t max_idx;
-  int8_t idx;
-  int8_t sp;
-  int8_t dir;
-};
+element_t SortBase::elements[N_COLUMNS];
+float SortBase::reads[N_COLUMNS];
+float SortBase::writes[N_COLUMNS];
+int SortBase::read_count = 0;
+int SortBase::write_count = 0;
 
-struct BubbleSortBase : public BubbleSortData, public SortBase {
-  virtual void init() {
-    SortBase::init();
+struct BubbleSortBase : public SortBase {
+  virtual void initImpl() {
     min_idx = 0;
     max_idx = N_COLUMNS - 1;
     idx = max_idx;
     sp = 1;
     dir = -1;
+    initBubbleImpl();
   }
+
+  virtual void initBubbleImpl() {}
 
   void compSwap(int _l, int _r) {
     if(dir * (get(_l) - get(_r)) > 0) {
@@ -92,79 +90,62 @@ struct BubbleSortBase : public BubbleSortData, public SortBase {
     }
   }
 
-  virtual void onBoundIdx() {}
-  virtual void onBoundBound() {}
-  virtual void onBoundDir() {}
-  virtual void onBoundSpace() {}
+  virtual void onBoundary() = 0;
 
-  virtual void step() {
+  virtual void stepImpl() {
     int n_idx = idx + dir * sp;
     compSwap(idx, n_idx);
     if(n_idx == (dir > 0 ? max_idx : min_idx)) {
-      onBoundBound();
-      onBoundIdx();
-      onBoundDir();
-      onBoundSpace();
+      onBoundary();
       if(max_idx - min_idx <= 1 || sp < 1) {
-        SetFinished(true);
+        setFinished(true);
       }
     } else {
       idx += dir;
     }
   }
 
-  BubbleSortData init_data;
   int8_t min_idx, max_idx, idx, sp, dir;
 };
 
 struct BubbleSort : public BubbleSortBase {
-  virtual void onBoundIdx() {idx = dir > 0 ? min_idx : max_idx;}
-  virtual void onBoundBound() {
+  virtual void onBoundary() {
     if(dir > 0) {
       max_idx -= 1;
     } else {
       min_idx += 1;
     }
+    idx = dir > 0 ? min_idx : max_idx;
   }
-  virtual void onBoundDir() {}
-  virtual void onBoundSpace() {}
 };
 
 struct CocktailSort : public BubbleSortBase {
-  virtual void onBoundIdx() {}
-  virtual void onBoundBound() {
+  virtual void onBoundary() {
     if(dir > 0) {
       max_idx -= 1;
     } else {
       min_idx += 1;
     }
+    dir *= -1;
   }
-  virtual void onBoundDir() {dir *= -1;}
-  virtual void onBoundSpace() {}
 };
 
 struct CombSort : public BubbleSortBase {
-  virtual void init() {
-    BubbleSortBase::init();
-    sp = N_COLUMNS - 1;
+  virtual void initBubbleImpl() {sp = N_COLUMNS - 1;}
+  virtual void onBoundary() {
+    idx = dir > 0 ? min_idx : max_idx;
+    sp -= 1;
   }
-
-  virtual void onBoundIdx() {idx = dir > 0 ? min_idx : max_idx;}
-  virtual void onBoundBound() {}
-  virtual void onBoundDir() {}
-  virtual void onBoundSpace() {sp -= 1;}
 };
 
 struct SelectionSort : public SortBase {
-
-  virtual void init() {
-    SortBase::init();
+  virtual void initImpl() {
     start_idx = 0;
     idx = 1;
     min_idx = 0;
   }
 
-  virtual void step() {
+  virtual void stepImpl() {
     if(idx < N_COLUMNS)
     {
       if(get(idx) < get(min_idx)) {
@@ -180,8 +161,8 @@ struct SelectionSort : public SortBase {
       ++start_idx;
       min_idx = start_idx;
       idx = start_idx + 1;
-      if(idx + 1 == N_COLUMNS) {
-        SetFinished(true);
+      if(idx == N_COLUMNS) {
+        setFinished(true);
       }
     }
   }
@@ -190,21 +171,19 @@ struct SelectionSort : public SortBase {
 };
 
 struct InsertionSort : public SortBase {
-
-  virtual void init() {
-    SortBase::init();
+  virtual void initImpl() {
     idx = 0;
     end_idx = 1;
     sortValue = get(end_idx);
   }
 
-  virtual void step() {
-    if(idx == -1 or get(idx) < sortValue)
+  virtual void stepImpl() {
+    if(idx == -1 || get(idx) <= sortValue)
     {
       set(idx + 1, sortValue);
       ++end_idx;
       if(end_idx == N_COLUMNS) {
-        SetFinished(true);
+        setFinished(true);
       } else {
         sortValue = get(end_idx);
         idx = end_idx - 1;
@@ -219,24 +198,57 @@ struct InsertionSort : public SortBase {
   element_t sortValue;
 };
 
-int shell_sort_sq[] = {701, 301, 132, 57, 23, 10, 4, 1};
+int shell_sort_sq[] = {7, 3, 1, 0};
 
-struct ShellSort : public BubbleSortBase {
-  virtual void init() {
-    BubbleSortBase::init();
+struct ShellSort : public SortBase {
+  bool updateIdx() {
+    if(end_idx + sp < N_COLUMNS) {
+      idx = end_idx;
+      end_idx += sp;
+    } else {
+      if(start_idx + 1 < sp) {
+        start_idx += 1;
+      } else if(sp > 1) {
+        sp = shell_sort_sq[++space_idx];
+        start_idx = 0;
+      } else {
+        return false;
+      }
+      idx = start_idx;
+      end_idx = start_idx + sp;
+    }
+    sortValue = get(end_idx);
+    return true;
+  }
+
+  virtual void initImpl() {
     space_idx = 0;
-    while(shell_sort_sq[space_idx] > N_COLUMNS - 1) {
+    while(shell_sort_sq[space_idx] * 2 > N_COLUMNS - 1) {
       ++space_idx;
     }
     sp = shell_sort_sq[space_idx];
+    start_idx = 0;
+    idx = 0;
+    end_idx = sp;
+    sortValue = get(end_idx);
   }
 
-  virtual void onBoundIdx() {idx = dir > 0 ? min_idx : max_idx;}
-  virtual void onBoundBound() {}
-  virtual void onBoundDir() {}
-  virtual void onBoundSpace() {sp = shell_sort_sq[++space_idx];}
+  virtual void stepImpl() {
+    if(idx < start_idx || get(idx) <= sortValue)
+    {
+      set(idx + sp, sortValue);
+      bool cont = updateIdx();
+      if(!cont) {
+        setFinished(true);
+      }
+    } else {
+      set(idx + sp, get(idx));
+      idx -= sp;
+    }
+  }
 
-  int space_idx;
+  int8_t idx, start_idx, end_idx, space_idx, sp;
+  element_t sortValue;
 };
 
 #endif
--- sorting_demo/sorting_demo.ino
+++ sorting_demo/sorting_demo.ino
@@ -18,12 +18,12 @@ uint8_t const led_on_high = 7u;
 BubbleSort bubbleSort;
 CombSort combSort;
 CocktailSort cocktailSort;
-ShellSort shellSort;
 SelectionSort selectionSort;
 InsertionSort insertionSort;
-SortBase * sorters[] = {&bubbleSort, &cocktailSort, &combSort, &insertionSort, &selectionSort};
-int num_sorters = 5;
-SortBase * sort = 0;
+ShellSort shellSort;
+SortBase * sorters[] = {&bubbleSort, &cocktailSort, &combSort, &insertionSort, &shellSort, &selectionSort};
+int const num_sorters = 6;
+int sort_idx = num_sorters - 1;
 
 void setup() {
   Serial.begin(9600);
@@ -31,15 +31,14 @@ void setup() {
   EEPROM.write(EEPROMaddress, (toggleState + 1) % 2); // Toggle the on/off state with each reset
   LedSign::Init(GRAYSCALE);
   elasped_time = millis();
-  sort = sorters[0];
 }
 
-void display(long _delay) {
+void display(SortBase * sort, long _delay) {
   for (uint8_t row = 0; row < N_ROWS; ++row) {
     for (uint8_t col = 0; col < N_COLUMNS; ++col) {
-      uint8_t val = row == elements[col]
-        ? writes[col] * led_on_high + (1.0f - writes[col]) * led_on
-        : reads[col] * led_off_high + (1.0f - reads[col]) * led_off;
+      uint8_t val = row == sort->elements[col]
+        ? sort->writes[col] * led_on_high + (1.0f - sort->writes[col]) * led_on
+        : sort->reads[col] * led_off_high + (1.0f - sort->reads[col]) * led_off;
       LedSign::Set(N_COLUMNS - 1 - col, row, val);
     }
   }
@@ -49,18 +48,18 @@ void display(long _delay) {
 void loop() {
   switch(toggleState){
   case 0:
-    decay_hightlights(0.5f);
+    sorters[sort_idx]->decayHightlights(0.5f);
 
-    if(sort->GetFinished()) {
-      display((write_time + read_time) * 10);
-      sort = sorters[random(0, num_sorters)];
-      sort->init();
-      display((write_time + read_time) * 10);
+    if(sorters[sort_idx]->getFinished()) {
+      display(sorters[sort_idx], (write_time + read_time) * 10);
+      sort_idx = (sort_idx + 1) % num_sorters;
+      sorters[sort_idx]->init();
+      display(sorters[sort_idx], (write_time + read_time) * 10);
     } else  {
-      sort->step();
+      sorters[sort_idx]->step();
     }
 
-    display(write_time * write_count + read_time * read_count);
+    display(sorters[sort_idx], write_time * sorters[sort_idx]->write_count + read_time * sorters[sort_idx]->read_count);
 
     elasped_time = millis();
     break;
